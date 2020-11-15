8b800cd0d6d7a241342192548dbdb5d32a28b109
Parth Vaidhya <pv.parth96@gmail.com>
2020-11-15 16:27:36 +0530

    Player information

---
+++ Snakes&Ladder.py
@@ -0,0 +1,13 @@
+
+# Player information and input
+def get_player_names():
+    player1_name = None
+    while not player1_name:
+        player1_name = input("Please enter a valid name for first player: ").strip()
+
+    player2_name = None
+    while not player2_name:
+        player2_name = input("Please enter a valid name for second player: ").strip()
+
+    print("\nMatch will be played between '" + player1_name + "' and '" + player2_name + "'\n")
+    return player1_name, player2_name
