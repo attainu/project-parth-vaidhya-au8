3218eb23d84fb37f967974861b112dcbf9ddbe83
Parth Vaidhya <pv.parth96@gmail.com>
2020-11-15 16:44:07 +0530

    Dice value

--- Snakes&Ladder.py
+++ Snakes&Ladder.py
@@ -3,11 +3,19 @@
 def get_player_names():
     player1_name = None
     while not player1_name:
-        player1_name = input("Please enter a valid name for first player: ").strip()
+        player1_name = input("Please enter your correct name player-1: ").strip()
 
     player2_name = None
     while not player2_name:
-        player2_name = input("Please enter a valid name for second player: ").strip()
+        player2_name = input("Please enter your correct name player-2: ").strip()
 
     print("\nMatch will be played between '" + player1_name + "' and '" + player2_name + "'\n")
     return player1_name, player2_name
+
+# number displayed on dice
+def get_dice_value():
+    time.sleep(SLEEP_BETWEEN_ACTIONS)
+    dice_value = random.randint(1, DICE_FACE)
+    print("You got  " + str(dice_value))
+    return dice_value 
+
