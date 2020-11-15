bc52caeacc0a9a926c301ed695787637d7548384
Parth Vaidhya <pv.parth96@gmail.com>
2020-11-15 17:43:12 +0530

    Specified value for snakes and ladder

--- Snakes&Ladder.py
+++ Snakes&Ladder.py
@@ -2,6 +2,48 @@ import time
 import random
 import sys
 
+SLEEP_BETWEEN_ACTIONS = 1
+MAX_VAL = 100
+DICE_FACE = 6
+
+# snake takes you down from 'key' to 'value'
+snakes = {
+    8: 4,
+    18: 1,
+    26: 10,
+    39: 5,
+    51: 6,
+    54: 36,
+    56: 1,
+    60: 23,
+    75: 28,
+    83: 45,
+    85: 59,
+    90: 48,
+    92: 25,
+    97: 87,
+    99: 63
+}
+
+# ladder takes you up from 'key' to 'value'
+ladders = {
+    3: 20,
+    6: 14,
+    11: 28,
+    15: 34,
+    17: 74,
+    22: 37,
+    38: 59,
+    49: 67,
+    57: 76,
+    61: 78,
+    73: 86,
+    81: 98,
+    88: 91
+}
+
+
+
 
 def welcome_msg():
     msg = """
